6d00a8f32ea6debc7e9735b17ffbe97597fde3c2
Bbeaurai <beaurainbenjamin1@gmail.com>
2026-06-08 16:42:47 +0200

    Add

--- src/parsing.py
+++ src/parsing.py
@@ -7,30 +7,50 @@
 #   By: bbeaurai <bbeaurai@student.42lehavre.fr>     +#+  +:+       +#+       #
 #                                                  +#+#+#+#+#+   +#+          #
 #   Created: 2026/06/05 09:58:26 by bbeaurai            #+#    #+#            #
-#   Updated: 2026/06/08 11:23:06 by bbeaurai           ###   ########.fr      #
+#   Updated: 2026/06/08 16:42:33 by bbeaurai           ###   ########.fr      #
 #                                                                             #
 # ########################################################################### #
 
 import json
 import os
 
-# verif answer est vide
+rs = "\033[0m"
+r = "\033[31m\033[5m\033[1m"
 
-_ROOT = os.path.join(os.path.dirname(__file__), "..")
+# verif answer est vide / permission
 
-def json_checker() -> bool:
+def json_checker(prompt_file: str, function_file: str) -> bool:
     try:
 
-        with open(os.path.join(_ROOT,
-                               "data/input/function_calling_tests.json"),
+        with open(os.path.join(f"data/input/{prompt_file}"),
                                "r") as f:
-            prompt = json.load(f)
-            print(prompt[0])
+            prompts = json.load(f)
+            for p in prompts:
+                print(p.keys(), p.values())
 
-    except (FileNotFoundError, json.decoder.JSONDecodeError, IndexError) as e:
-        print(e)
+                if (p.keys() != "prompt" and len(p.values()) <= 0):
+                    raise ValueError(f"prompt ({p}) is not in the "
+                                     "correct format")
+
+    except (FileNotFoundError, json.decoder.JSONDecodeError,
+            IndexError, PermissionError) as e:
+        print(f"{r}[ERROR]{rs}: {e}")
+        return (False)
+
+    except ValueError as e:
+        print(f"{r}[ERROR]{rs}: {e}")
         return (False)
 
     return (True)
 
-json_checker()
+def parser(pf: str, ff: str, ) -> bool:
+
+    prompt_file = pf
+    function_file = ff
+
+    if (json_checker(prompt_file, function_file)):
+        return (True)
+    return (False)
+
+
+print(parser("function_calling_tests.json", "functions_definition.json"))
